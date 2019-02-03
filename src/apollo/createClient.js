a66684f02b684a1f83277aface919df2207f6686
Mikhail Cheberya <mail@idope.ru>
2019-02-04 01:30:14 +0300

    Created fragment types file for GitHub API

--- src/apollo/createClient.js
+++ src/apollo/createClient.js
@@ -1,6 +1,18 @@
 import ApolloClient from 'apollo-boost';
+import {
+	IntrospectionFragmentMatcher,
+	InMemoryCache
+} from 'apollo-cache-inmemory';
+import introspectionQueryResultData from './fragmentTypes.json';
+
+const fragmentMatcher = new IntrospectionFragmentMatcher({
+	introspectionQueryResultData
+});
+
+const cache = new InMemoryCache({ fragmentMatcher });
 
 const client = new ApolloClient({
+	cache,
 	uri: 'https://api.github.com/graphql',
 	request: async operation => {
 		const token = process.env.PREACT_APP_GITHUB_TOKEN;
