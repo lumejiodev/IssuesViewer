06896fa79e29fcd87cb9fe547f841f70f6090d08
Mikhail Cheberya <mail@idope.ru>
2019-02-06 00:46:53 +0300

    `preact build` fixes for async/await and GraphQL

--- preact.config.js
+++ preact.config.js
@@ -1,5 +1,6 @@
 import envVars from 'preact-cli-plugin-env-vars';
 
 export default function(config, env, helpers) {
+	config.resolve.extensions.unshift('.mjs'); // for GraphQL
 	envVars(config, env, helpers);
 }
